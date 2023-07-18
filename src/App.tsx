3f55a43633f8fdb3de2a78a59766bb520bc9ebcd
tomasleggire <tomasleggire@hotmail.com>
2023-07-18 16:19:24 -0300

    migracion a StepsLayouts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import StepsMain from './containers/StepsMain';
 import useSteps from './hooks/useSteps';
 import FormLayout from './containers/FormLayout';
-import InputText from './components/InputText';
+import StepsLayouts from './containers/StepsLayouts';
 
 function App() {
   const {
@@ -21,40 +21,15 @@ function App() {
     <div className="App">
       <StepsMain steps={steps} />
       <FormLayout>
-        <div>
-          <h1>Personal info</h1>
-          <h3>Provide your name, email address and phone number.</h3>
-          <InputText
-            label="Name"
-            type="text"
-            placeholder="e.g. Stephen King"
-            min={5}
-            max={35}
-            value={name}
-            setValue={setName}
-          />
-          <InputText
-            label="Email Address"
-            type="email"
-            placeholder="e.g. stephenking@lorem.com"
-            min={10}
-            max={35}
-            value={email}
-            setValue={setEmail}
-          />
-          <InputText
-            label="Phone Number"
-            type="number"
-            placeholder="e.g. +1122334455"
-            min={0}
-            max={11}
-            value={phoneNumber}
-            setValue={setPhoneNumber}
-          />
-        </div>
-        <button className="form-btn" type="button" onClick={handleClickStep1}>
-          Next Step
-        </button>
+        <StepsLayouts
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          phoneNumber={phoneNumber}
+          setPhoneNumber={setPhoneNumber}
+          handleClickStep1={handleClickStep1}
+        />
       </FormLayout>
     </div>
   );
---
+++ src/containers/StepsLayouts.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import InputText from '../components/InputText';
+import type { SetStateAction } from 'react';
+
+interface MyProps {
+  name: string;
+  setName: React.Dispatch<SetStateAction<string>>;
+  email: string;
+  setEmail: React.Dispatch<SetStateAction<string>>;
+  phoneNumber: number;
+  setPhoneNumber: React.Dispatch<SetStateAction<number>>;
+  handleClickStep1: () => void;
+}
+
+export default function StepsLayouts(props: MyProps) {
+  return (
+    <>
+      {' '}
+      <div>
+        <h1>Personal info</h1>
+        <h3>Provide your name, email address and phone number.</h3>
+        <InputText
+          label="Name"
+          type="text"
+          placeholder="e.g. Stephen King"
+          min={5}
+          max={35}
+          value={props.name}
+          setValue={props.setName}
+        />
+        <InputText
+          label="Email Address"
+          type="email"
+          placeholder="e.g. stephenking@lorem.com"
+          min={10}
+          max={35}
+          value={props.email}
+          setValue={props.setEmail}
+        />
+        <InputText
+          label="Phone Number"
+          type="number"
+          placeholder="e.g. +1122334455"
+          min={0}
+          max={11}
+          value={props.phoneNumber}
+          setValue={props.setPhoneNumber}
+        />
+      </div>
+      <button
+        className="form-btn"
+        type="button"
+        onClick={props.handleClickStep1}
+      >
+        Next Step
+      </button>
+    </>
+  );
+}
