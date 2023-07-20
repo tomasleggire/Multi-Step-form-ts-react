70e551d65370155cb68bd113a59b696e0da81f5f
tomasleggire <tomasleggire@hotmail.com>
2023-07-20 16:38:12 -0300

    ToggleSwitch component terminado

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -2,19 +2,23 @@ import React, { useState } from 'react';
 import '../styles/ToggleSwitch.css';
 
 function ToggleSwitch() {
-  const [toggleState, setToggleState] = useState(false);
+  const [toggleState, setToggleState] = useState(true);
 
   const handleToggle = () => {
     setToggleState(!toggleState);
-    console.log('Toggle State:', toggleState ? 'Monthly' : 'Yearly');
+    console.log('Toggle State:', toggleState ? 'Yearly' : 'Monthly');
   };
 
   return (
     <div className="toggle-wrapper">
+      <div className={`label ${toggleState ? 'selected' : ''}`}>Monthly</div>
       <label className="switch">
         <input type="checkbox" onChange={handleToggle} />
         <span className="slider round"></span>
       </label>
+      <div className={`label year ${toggleState ? '' : 'selected'}`}>
+        Yearly
+      </div>
     </div>
   );
 }
